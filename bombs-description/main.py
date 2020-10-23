cde01f9fa18bd6444d7297d78bee567f7d73ee9b
Immanuela Busari <immanuela.busari@ad.mentormate.bg>
2020-10-24 00:47:38 +0300

    Initial commit

---
+++ bombs-description/main.py
@@ -0,0 +1,8 @@
+# import numpy as np
+
+
+bomb_effects = list(int(num) for num in input(f"Bomb effects: ").split(','))
+bomb_casings = list(int(num) for num in input(f"Bomb casings: ").split(','))
+
+print(bomb_effects)
+print(bomb_casings)
